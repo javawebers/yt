ed8d8fec334e11c0a42e00fee579761dacb3d4f6
likeabook <likeabook@163.com>
2020-03-31 21:19:46 +0800

    1.BaseAccidentException 和 BaseErrorException 无参构造函数。 2.发布 1.3.2 版本

--- src/main/java/com/github/yt/commons/exception/BaseAccidentException.java
+++ src/main/java/com/github/yt/commons/exception/BaseAccidentException.java
@@ -8,8 +8,8 @@ package com.github.yt.commons.exception;
  * <p>
  * 认为是（Is considered to be）	    设计的一部分	                难以应付的意外
  * 预期发生（Is expected to happen）	有规律但很少发生	            从不
- * 谁来处理（Who cares about it）	    调用方法的上游代码	            需要修复此问题的人员
- * 实例（Examples）	                另一种返回模式	                编程缺陷，硬件故障，配置错误，文件丢失，服务器无法使用
+ * 谁来处理（Who cares about it）	调用方法的上游代码	        需要修复此问题的人员
+ * 实例（Examples）	                另一种返回模式	            编程缺陷，硬件故障，配置错误，文件丢失，服务器无法使用
  * 最佳映射（Best Mapping）	        已检查异常	                未检查异常
  *
  * @author liujiasheng
