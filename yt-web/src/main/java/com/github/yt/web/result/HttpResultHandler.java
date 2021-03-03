3bb906fb66633056da2e99c06b6c6c9abb5e0c8b
liujiasheng <likeabook@163.com>
2021-03-03 11:53:28 +0800

    发布 2.1.18，异常时，支持把异常信息写到 header 中，经过base64转码，解决汉字乱码问题

--- yt-web/src/main/java/com/github/yt/web/result/HttpResultHandler.java
+++ yt-web/src/main/java/com/github/yt/web/result/HttpResultHandler.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Objects;
 
@@ -28,6 +29,9 @@ import static com.github.yt.web.result.PackageResponseBodyAdvice.REQUEST_RESULT_
  */
 public class HttpResultHandler {
     private static final Logger logger = LoggerFactory.getLogger(HttpResultHandler.class);
+
+    public static final String HEADER_BUSINESS_EXCEPTION_RESULT_BODY = "Business-Exception-Result-Body";
+    public static final String HEADER_BUSINESS_OCCUR_EXCEPTION = "Business-Occur-Exception";
 
     private static volatile BaseResultConfig resultConfig;
 
@@ -65,10 +69,13 @@ public class HttpResultHandler {
      * @param moreResult 更多结果
      * @return HttpResultEntity
      */
-    private static HttpResultEntity getSuccessMoreResultBody(Object result, boolean withMore, Object moreResult) {
+    private static HttpResultEntity getSuccessMoreResultBody(Object result, boolean withMore,
+            Object moreResult) {
         HttpResultEntity resultBody = new HttpResultEntity();
-        resultBody.put(getResultConfig().getErrorCodeField(), getResultConfig().getDefaultSuccessCode());
-        resultBody.put(getResultConfig().getMessageField(), getResultConfig().getDefaultSuccessMessage());
+        resultBody.put(getResultConfig().getErrorCodeField(),
+                getResultConfig().getDefaultSuccessCode());
+        resultBody.put(getResultConfig().getMessageField(),
+                getResultConfig().getDefaultSuccessMessage());
         resultBody.put(getResultConfig().getResultField(), result);
         setExpandField(resultBody);
         if (withMore) {
@@ -79,17 +86,22 @@ public class HttpResultHandler {
 
     /**
      * 设置 response 到 header 中，方便 feign 调用进行统一判断
+     *
      * @param resultBody 返回的 map 对象
-     * @param response response
+     * @param response   response
      */
-    public static void setResponseToHeader(HttpResultEntity resultBody, HttpServletResponse response, Throwable exception) {
+    public static void setResponseToHeader(HttpResultEntity resultBody,
+            HttpServletResponse response, Throwable exception) {
         YtWebConfig ytWebConfig = SpringContextUtils.getBean(YtWebConfig.class);
         boolean setResponseToHeader = ytWebConfig.getResult().isSetResponseToHeader();
         if (setResponseToHeader) {
             HttpResultEntity headerResultBody = new HttpResultEntity();
-            headerResultBody.put(getResultConfig().getErrorCodeField(), resultBody.get(getResultConfig().getErrorCodeField()));
-            headerResultBody.put(getResultConfig().getMessageField(), resultBody.get(getResultConfig().getMessageField()));
-            headerResultBody.put(getResultConfig().getResultField(), resultBody.get(getResultConfig().getResultField()));
+            headerResultBody.put(getResultConfig().getErrorCodeField(),
+                    resultBody.get(getResultConfig().getErrorCodeField()));
+            headerResultBody.put(getResultConfig().getMessageField(),
+                    resultBody.get(getResultConfig().getMessageField()));
+            headerResultBody.put(getResultConfig().getResultField(),
+                    resultBody.get(getResultConfig().getResultField()));
             setExpandField(headerResultBody);
 
             boolean setStackTraceToHeader = ytWebConfig.getResult().isSetStackTraceToHeader();
@@ -97,9 +109,12 @@ public class HttpResultHandler {
             if (setStackTraceToHeader) {
                 headerResultBody.put(stackTraceField, getAndSetExceptionStrToRequest(exception));
             }
-            response.addHeader("ExceptionResultBody", JsonUtils.toJsonString(headerResultBody));
-        }
 
+            byte[] headerResultBodyByte = Base64.getEncoder()
+                    .encode(JsonUtils.toJsonString(headerResultBody).getBytes());
+            response.addHeader(HEADER_BUSINESS_EXCEPTION_RESULT_BODY, new String(headerResultBodyByte));
+            response.addHeader(HEADER_BUSINESS_OCCUR_EXCEPTION, "true");
+        }
 
     }
 
@@ -113,12 +128,15 @@ public class HttpResultHandler {
         HttpResultEntity resultBody = new HttpResultEntity();
         if (exception instanceof BaseException) {
             BaseException baseException = (BaseException) exception;
-            resultBody.put(getResultConfig().getErrorCodeField(), getResultConfig().convertErrorCode(baseException.getErrorCode()));
+            resultBody.put(getResultConfig().getErrorCodeField(),
+                    getResultConfig().convertErrorCode(baseException.getErrorCode()));
             resultBody.put(getResultConfig().getMessageField(), exception.getMessage());
             resultBody.put(getResultConfig().getResultField(), baseException.getErrorResult());
         } else {
-            resultBody.put(getResultConfig().getErrorCodeField(), getResultConfig().getDefaultErrorCode());
-            resultBody.put(getResultConfig().getMessageField(), getResultConfig().getDefaultErrorMessage());
+            resultBody.put(getResultConfig().getErrorCodeField(),
+                    getResultConfig().getDefaultErrorCode());
+            resultBody.put(getResultConfig().getMessageField(),
+                    getResultConfig().getDefaultErrorMessage());
             resultBody.put(getResultConfig().getResultField(), null);
         }
         setExpandField(resultBody);
@@ -126,7 +144,8 @@ public class HttpResultHandler {
         // 返回异常堆栈到前端
         YtWebConfig ytWebConfig = SpringContextUtils.getBean(YtWebConfig.class);
         String stackTraceField = getResultConfig().getStackTraceField();
-        if (ytWebConfig.getResult().isReturnStackTrace() && YtStringUtils.isNotBlank(stackTraceField)) {
+        if (ytWebConfig.getResult().isReturnStackTrace() && YtStringUtils
+                .isNotBlank(stackTraceField)) {
             resultBody.put(stackTraceField, getAndSetExceptionStrToRequest(exception));
         }
         return resultBody;
@@ -151,7 +170,6 @@ public class HttpResultHandler {
         return exceptionStr;
     }
 
-
     /**
      * 设置扩展字段
      * 包含 uuid、请求时间、响应时间
@@ -161,11 +179,13 @@ public class HttpResultHandler {
     private static void setExpandField(HttpResultEntity resultBody) {
         String uuidField = getResultConfig().getUuidField();
         if (YtStringUtils.isNotBlank(uuidField)) {
-            resultBody.put(uuidField, getRequest().getAttribute(RequestHandlerInterceptor.REQUEST_UUID));
+            resultBody.put(uuidField,
+                    getRequest().getAttribute(RequestHandlerInterceptor.REQUEST_UUID));
         }
         String requestTimeField = getResultConfig().getRequestTimeField();
         if (YtStringUtils.isNotBlank(requestTimeField)) {
-            resultBody.put(requestTimeField, getRequest().getAttribute(RequestHandlerInterceptor.REQUEST_TIME));
+            resultBody.put(requestTimeField,
+                    getRequest().getAttribute(RequestHandlerInterceptor.REQUEST_TIME));
         }
         String responseTimeField = getResultConfig().getResponseTimeField();
         if (YtStringUtils.isNotBlank(responseTimeField)) {
@@ -173,7 +193,8 @@ public class HttpResultHandler {
         }
     }
 
-    public static void writeExceptionResult(final Throwable e, HttpServletRequest request, HttpServletResponse response) {
+    public static void writeExceptionResult(final Throwable e, HttpServletRequest request,
+            HttpServletResponse response) {
         // 当不向上抛异常时主动打印异常
         logger.error(e.getMessage(), e);
         HttpResultEntity resultBody = HttpResultHandler.getErrorSimpleResultBody(e);
@@ -188,7 +209,6 @@ public class HttpResultHandler {
             throw new RuntimeException(ioException);
         }
     }
-
 
     private static HttpServletRequest getRequest() {
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
