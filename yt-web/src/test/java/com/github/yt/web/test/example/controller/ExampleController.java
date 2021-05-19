d279cda78a70f1d9a9c5298b6b53bdf4fd876d66
liujiasheng <likeabook@163.com>
2021-05-19 15:39:32 +0800

    // 方法中传入 response 对象，同时返回值为 void 时，不会进行自动包装         // 可以采用注入 response 对象方式

--- yt-web/src/test/java/com/github/yt/web/test/example/controller/ExampleController.java
+++ yt-web/src/test/java/com/github/yt/web/test/example/controller/ExampleController.java
@@ -3,18 +3,34 @@ package com.github.yt.web.test.example.controller;
 import com.github.yt.commons.exception.BaseAccidentException;
 import com.github.yt.web.test.exception.MyBusinessExceptionEnum;
 import com.github.yt.web.test.query.WebQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class ExampleController {
+    @Autowired
+    HttpServletResponse response;
 
     @GetMapping("autoPackageVoid")
     public void autoPackageVoid() {
 
+    }
+
+    @GetMapping("autoPackageResponse")
+    public void autoPackageResponse(HttpServletResponse response) {
+        // 方法中传入 response 对象，同时返回值为 void 时，不会进行自动包装
+        // 可以采用注入 response 对象方式
+    }
+
+    @GetMapping("autoPackageResponse2")
+    public void autoPackageResponse2() {
+
+        System.out.println(response);
     }
 
     @GetMapping("autoPackageResult")
