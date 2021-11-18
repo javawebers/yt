46c1ed54cd5a01b25485c726783c6348fc40d52d
liujiasheng <likeabook@163.com>
2021-11-18 17:21:03 +0800

    发布3.0.0.适配springboot 2.5.6。

--- yt-web/src/test/java/com/github/yt/web/test/YtWebTestApplication.java
+++ yt-web/src/test/java/com/github/yt/web/test/YtWebTestApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 @EnableYtWebTest
 //@AutoConfigureMockMvc
 @Controller
